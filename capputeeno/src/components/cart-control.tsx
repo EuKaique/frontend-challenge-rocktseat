380ab39393d90eb53b16a5dd49ce0594c28f0173
Kaique <kaique.santos@duosystem.com.br>
2023-08-04 13:05:50 -0300

    feat: creating header

---
+++ capputeeno/src/components/cart-control.tsx
@@ -0,0 +1,29 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { styled } from "styled-components";
+import { CartIcon } from "./cart-icon";
+
+const CartCount = styled.span`
+    width: 17px;
+    height: 17px;
+    border-radius: 50%;
+    font-size: 10px;
+    padding: 1.5px 6px;
+    background-color: #DE3838;
+    color: #FFF;
+
+    margin-left: -8px;
+`
+
+const Container = styled.div`
+    position: relative;
+`
+
+export function CartControl(){
+    const { value } = useLocalStorage('cart-items')
+    return (
+        <Container>
+            <CartIcon />
+            {value.length && <CartCount>{value.length}</CartCount>}
+        </Container>
+    )
+}
---
+++ capputeeno/src/components/primary-input.tsx
@@ -0,0 +1,46 @@
+import { InputHTMLAttributes } from "react";
+import { styled } from "styled-components";
+import { SearchIconSvg } from "./search-icon";
+
+export const PrimaryInput = styled.input`
+    font-family: inherit;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+    letter-spacing: 0em;
+    text-align: center;
+
+    width: 352px;
+    padding: 10px 16px;
+    border-radius: 8px;
+    border: none;
+    outline: none;
+
+    background-color: var(--bg-secondary);
+    color: var(--text-dark);
+`
+
+export const InputContainer = styled.div`
+    position: relative;
+    width: 352px;
+
+    svg{
+        position: absolute;
+        right: 20px;
+        top: 50%;
+        transform: translateY(-50%);
+
+        cursor: pointer;
+    }
+`
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{}
+
+export function PrimaryInputWSearchIcon(props: InputProps){
+    return(
+         <InputContainer>
+            <PrimaryInput {...props} />
+            <SearchIconSvg />
+         </InputContainer>
+    )
+}
